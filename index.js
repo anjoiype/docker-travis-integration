86702d9946c21a07b567f035b9e863708f9c1687
Anjo Joseph <ANJ@sitecore.net>
2020-06-10 14:27:51 +0800

    comit for testing kubernetes #3

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I have sucessfully integrated CI/CD testing #2");
+    res.json("I have sucessfully integrated CI/CD testing #3");
 });
 
 app.listen(PORT, () => {
