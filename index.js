cc4cb7591bcead310a18b7d0d1abef76a4070a96
Anjo Joseph <ANJ@sitecore.net>
2020-06-10 14:59:53 +0800

    testing commit #5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.connect(config.DB, { useNewUrlParser: true }, (err, db) => {
 });
 
 app.get('/', (req, res) => {
-    res.json("I have sucessfully integrated CI/CD testing #4");
+    res.json("I have sucessfully integrated CI/CD testing #5");
 });
 
 app.listen(PORT, () => {
